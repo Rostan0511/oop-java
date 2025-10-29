b275e1f513aaaa6fa7f2bc34fc4b36f11977a065
Rostan0511 <loborostan05@gmail.com>
2025-10-29 11:46:08 +0530

    class creation

---
+++ src/introduction/Main.java
@@ -0,0 +1,17 @@
+package introduction;
+
+public class Main {
+    public static void main(String[] args) {
+        // store 5 num
+        int[] numbers = {1,2,3,4,5};
+        // store 5 names
+        String[] names = new String[5];
+
+        Student[] students = new Student[5];
+    }
+    class Student {
+        int[] num = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+    }
+}
